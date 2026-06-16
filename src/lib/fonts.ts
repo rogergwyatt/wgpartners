adece80b40763ed1e95d9f9b210fd994469f45d4
Roger Wyatt <rogergwyatt@gmail.com>
2026-06-16 09:52:54 -0400

    feat: add Playfair Display + Inter font module

---
+++ src/lib/fonts.ts
@@ -0,0 +1,14 @@
+import { Inter, Playfair_Display } from 'next/font/google';
+
+export const sans = Inter({
+  subsets: ['latin'],
+  variable: '--font-sans',
+  display: 'swap',
+});
+
+export const serif = Playfair_Display({
+  subsets: ['latin'],
+  variable: '--font-serif',
+  display: 'swap',
+  weight: ['400', '700', '800'],
+});
